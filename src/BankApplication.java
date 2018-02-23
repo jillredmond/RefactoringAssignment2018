8fbd73f4d73ea3c852900a21d84391bbb76695b0
jillredmond <redmondjill@hotmail.com>
2018-02-23 20:47:51 +0000

    final commit

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -381,8 +381,7 @@ public class BankApplication extends JFrame {
 		
 				JFrame frame = new JFrame("TableDemo");
 				JPanel pan = new JPanel();
-			
-		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 				String col[] = {"ID","Number","Name", "Account Type", "Balance", "Overdraft"};
 				
 				DefaultTableModel tableModel = new DefaultTableModel(col, 0);
